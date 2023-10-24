afb7864cfbf8e9d60a4e44cd2847818681ccb078
Natalia Rosa <nataliashelnutt@gmail.com>
2023-10-24 09:48:36 -0300

    Aprendendo sobre operadores, convenção de código, tipos primitivos, strings, text blocks, formatação de texto etc..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) / 3;
+        //Média calculada pelas 3 notas de Fernando, Matheus e Igor
     }
 }
